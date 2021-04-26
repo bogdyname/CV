bf62f7e4f293d975c6ac12aa6920db54ba78f18f
bogdyname <bogdyname@gmail.com>
2021-04-26 23:26:00 +0300

    added graph class

--- Graphs/Graphs/Graphs.cpp
+++ Graphs/Graphs/Graphs.cpp
@@ -2,19 +2,34 @@
 
 using namespace std;
 
-int main()
+Graphs::Graphs(float x = 1.0, float y = 1.0) :
+    x(x), y(y)
+{
+
+}
+
+Graphs::~Graphs()
+{
+    ReleaseDC(hwnd, hdc);
+}
+
+bool Graphs::InitWindowsApi()
 {
     HWND hwnd = GetConsoleWindow();
     HDC hdc = GetDC(hwnd);
+}
 
-    int x = 0;
-    for (float i = 0; i < 3.14 * 10; i += 0.05)
+bool Graphs::DrawGraph()
+{
+    for (float iter = 0; iter < 3.14 * 10; iter += 0.05)
     {
-        SetPixel(hdc, x, 50 + 25 * cos(i), RGB(50, 200, 150));
-        x += 1;
+        SetPixel(hdc, x, 50 + 25 * cos(iter), RGB(50, 200, 150));
+        x += 0.1;
     }
+}
 
-    ReleaseDC(hwnd, hdc);
+int main()
+{
     cin.ignore();
 
     return 0;
--- Graphs/Graphs/Graphs.h
+++ Graphs/Graphs/Graphs.h
@@ -3,3 +3,21 @@
 #include <windows.h>
 #include <iostream>
 #include <cmath>
+
+class Graphs
+{
+public:
+	Graphs(float x = 1.0, float y = 1.0);
+	~Graphs();
+
+public:
+	bool InitWindowsApi();
+	bool DrawGraph();
+
+private:
+	float x;
+	float y;
+
+	HWND hwnd = NULL;
+	HDC hdc = NULL;
+};
