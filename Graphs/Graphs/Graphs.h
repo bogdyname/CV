62f9fd7cc1a34d253c03ac20109895b75f84e9df
bogdyname <bogdyname@gmail.com>
2021-05-04 21:39:44 +0300

    delete test

--- Graphs/Graphs/Graphs.h
+++ Graphs/Graphs/Graphs.h
@@ -1,23 +1,2 @@
-﻿#pragma once
-
-#include <windows.h>
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
-
-class Graphs
-{
-public:
-	Graphs(float x = 1.0, float y = 1.0);
-	~Graphs();
-
-public:
-	bool InitWindowsApi();
-	bool DrawGraph();
-
-private:
-	float x;
-	float y;
-
-	HWND hwnd = NULL;
-	HDC hdc = NULL;
-};
--- Graphs/main.cpp
+++ Graphs/main.cpp
@@ -5,12 +5,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    float x = 15;
-    float y = 5;
-
-    Graphs graphs(x, y);
-    graphs.InitWindowsApi();
-    graphs.DrawGraph();
 
     return 0;
 }
