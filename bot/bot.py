a92f04b6ecd8dcf72094b92beff69934f3d42229
bogdyname <bogdyname@gmail.com>
2022-08-07 18:54:11 +0300

    feature: bot
    -- added create and check bot object
    -- update log object. create it in init method to use in other methods of class
    -- added default check methods of agrs

--- bot/bot.py
+++ bot/bot.py
@@ -2,34 +2,50 @@ import asyncio
 from telethon.sync import TelegramClient, events
 
 class Bot:
-	def __init__(self, api_id='', api_hash=''):
-		print('Create bot.')
+	def __init__(self, logs=None, api_id='', api_hash=''):
+		print('Creating bot.')
+
+		if logs is None:
+			print('Error: Logger is None! (Bot)')
+			return False
+
 		# array to save id of users
 		self.user_id_list = []
 		self.api_id = api_id
 		self.api_hash = api_hash
+		self.logs = logs
+
+		# create telegram client
+		self.client = None
+		self.client = TelegramClient(username, api_id, api_hash)
+		
+		if client is None:
+			logs.error('Cannot create telegram client')
+			return None
+		else:
+			logs.error('Created telegram client')
 
 	def RunH8Chat(self, username='', api_id='', api_hash=''):
 		if not username or not self.api_id or not self.api_hash:
-			log.error('Confgi data for chat is bad.')
-			log.error('User data: ', username)
-			log.error('API id: ', api_id)
-			log.error('API hash: ', api_hash)
+			self.logs.critical('Data for chat bot is bad (Bot).')
+			self.logs.critical('User data: ', username)
+			self.logs.critical('API id: ', api_id)
+			self.logs.critical('API hash: ', api_hash)
 			return False
 
-	def Statistic(self, logger=None, user_id='', data=''):
-		if logger is None:
-			print('Logger for statistic is None')
+	def StopH8Chat(self, str_info=''):
+		if not str_info:
+			self.logs.critical('Why did you stop me (Bot)?')
 			return False
 
+	def Statistic(self, user_id='', data=''):
 		if not user_id:
-			logger.critical('User id is empty!')
+			self.logs.critical('User id is empty! (Bot)')
 			return False
 
 		if not data:
-			logger.critical('Data of statistic is empty!')
+			self.logs.critical('Data of statistic is empty! (Bot)')
 			return False
 
-		log.info('Write statistic of selected user.')
-
+		self.logs.info('Write statistic of selected user. (Bot)')
 		return True
