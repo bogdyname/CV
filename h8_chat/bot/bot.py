57fe500d55691b906eb7b21ac3a4496303a2dc8d
bogdyname <bogdyname@gmail.com>
2023-10-30 01:04:00 +0300

    shift bot src
    -- shift code of bot to it own source class
    -- clear main file
    --delete trash prints
    -- added init files for dirs

--- h8_chat/bot/bot.py
+++ h8_chat/bot/bot.py
@@ -2,50 +2,53 @@ import asyncio
 from telethon.sync import TelegramClient, events
 
 class Bot:
-	def __init__(self, logs=None, api_id='', api_hash=''):
-		print('Creating bot.')
-
+	def __init__(self, logs=None, username='', api_id='', api_hash=''):
 		if logs is None:
-			print('Error: Logger is None! (Bot)')
-			return False
+			raise Exception('Error: Logger is None! (Bot)')
 
 		# array to save id of users
 		self.user_id_list = []
+		self.username = username
 		self.api_id = api_id
 		self.api_hash = api_hash
 		self.logs = logs
 
 		# create telegram client
 		self.client = None
-		self.client = TelegramClient(username, api_id, api_hash)
+		self.client = TelegramClient(self.username, self.api_id, self.api_hash)
 		
-		if client is None:
-			logs.error('Cannot create telegram client')
-			return None
+		if self.client is None:
+			raise Exception('Cannot create telegram client')
 		else:
 			logs.error('Created telegram client')
 
-	def RunH8Chat(self, username='', api_id='', api_hash=''):
-		if not username or not self.api_id or not self.api_hash:
-			self.logs.critical('Data for chat bot is bad (Bot).')
-			self.logs.critical('User data: ', username)
-			self.logs.critical('API id: ', api_id)
-			self.logs.critical('API hash: ', api_hash)
-			return False
+	def run_H8_chat(self):
+		self.client.start()
 
-	def StopH8Chat(self, str_info=''):
-		if not str_info:
-			self.logs.critical('Why did you stop me (Bot)?')
-			return False
+		#event on new message
+		@client.on(events.NewMessage(incoming=True, forwards=None))
+		async def handler(event):
+			user_info = event.message.to_dict()['from_id']
+			user_id = user_info['user_id']
+			user_name = event.message
 
-	def Statistic(self, user_id='', data=''):
-		if not user_id:
-			self.logs.critical('User id is empty! (Bot)')
-			return False
+			count = 0
+			for i in self.user_id_list:
+				if i == user_id:
+					count += 1
+				else:
+					continue
 
-		if not data:
-			self.logs.critical('Data of statistic is empty! (Bot)')
-			return False
+			if count == 0:
+				self.user_id_list.append(user_id)
+				await asyncio.sleep(1)
+				await self.client.send_message(user_id, 'Text')
+			print(self.user_id_list)
+		
+		self.client.run_until_disconnected()
 
-		self.logs.info('Write statistic of selected user. (Bot)')
-		return True
+	def stop_H8_chat(self):
+		self.client.disconnect()
+
+	def statistic(self):
+		pass
